ad6a4bc5b611f2f4e08355f13781957389d804f5
Your Name <you@example.com>
2019-07-30 12:03:18 +0800

    nuxt项目结构基本完成

---
+++ components/header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="header">
+      
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+  .main {
+    width:1000px;
+    margin:0 auto;
+  }
+  .logo {
+    img {
+      width: 156px;
+    }
+  }
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+  /* 初始化 */
+  *{
+    margin:0;
+    padding:0;
+  }
+  /* 去掉小点点 */
+  ul,ol,li {
+    list-style:none;
+  }
+  /* a标签的颜色继承和去掉下划线 */
+  a {
+    color:inherit;
+    text-decoration: none;
+  }
+  /* 去掉字体斜体 */
+  em,i {
+    font-style:normal;
+  }
+  /* 去掉外边框和外轮廓线 */
+  input {
+    border:none;
+    outline:none;
+  }
+</style>
